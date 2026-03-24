3b406bb4f207a7b624ce476cbe6ae6f9abb4406a
Aniruddh <anilkharabe@gmail.com>
2026-03-24 09:02:20 +0530

    parcel dependencies and npm installations

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom/client'
+
+ const paragraph = React.createElement('p', {} , 'This is p from React');
+ 
  const parent = React.createElement('div', { id:'parent1'}, 
                         React.createElement('div', { id:'parent2'}, 
                             [
-                                React.createElement('h1', {id:"heading"}, "I'am in Heading Tag"),
+                                React.createElement('h1', {id:"heading"}, "I'am in Heading Tag testing    "),
                                 React.createElement('h2', {id:"heading"}, "I'am in Heading 2 Tag")
                             ]
                             
@@ -9,6 +14,7 @@
                     )
 
     console.log('parent', parent) // object
+    console.log('paragraph', paragraph)
     const rootReact = ReactDOM.createRoot(document.getElementById('root'));
 
     rootReact.render(parent)// takes object and convert it into html element
