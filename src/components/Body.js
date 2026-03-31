6d492230e47c75856f11ef6c19a278b9948539a8
Aniruddh <anilkharabe@gmail.com>
2026-03-31 09:03:18 +0530

    useState hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resInfoList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = ()=>{
+  let [listOfRestaurantsWithState, setListOfRestaurantsWithState ] = useState(resInfoList)
+
+  let listOfRestaurants = resInfoList;
+
+
   return(
     <div className="body">
-      <div className="m-[15px] p-[15px]">Search</div>
+      <div>
+        <div className="m-[15px] p-[15px]">Search</div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={()=>{
+          listOfRestaurantsWithState = listOfRestaurantsWithState.filter((res)=>{
+            return res.info.avgRating > 4.5;
+          });
+          setListOfRestaurantsWithState(listOfRestaurantsWithState)
+          
+        }}>Top Rated Restaurants</button>
+
+      </div>
       <div className="flex flex-wrap m-[20px]">
-        { resInfoList.map((currentRestaurant) => <RestaurantCard key={currentRestaurant.info.id} resObj = {currentRestaurant}/>)}
+        { listOfRestaurantsWithState.map((currentRestaurant) => <RestaurantCard key={currentRestaurant.info.id} resObj = {currentRestaurant}/>)}
       </div>
     </div>
   )
