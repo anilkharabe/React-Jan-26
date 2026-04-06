f9c358ef00631a3ab2a759f8fa3d5c075aca01c2
Aniruddh <anilkharabe@gmail.com>
2026-04-06 08:54:34 +0530

    updated the feature of search restaurant

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,8 @@ import Shimmer from "./Shimmer";
 const Body = ()=>{
 
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
-
+  const [searchText, setSearchText] = useState('');
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]); 
 
   useEffect(()=>{
     fetchData()
@@ -15,28 +16,41 @@ const Body = ()=>{
   const fetchData =  async()=>{
     const data = await axios.get('https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5986763&lng=73.79783479999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
     setListOfRestaurants(data?.data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredRestaurant(data?.data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
 
-  if(listOfRestaurants.length ==  0){
-    return <Shimmer />
-  }
+  // Conditional rendering
+  return listOfRestaurants.length ==  0 ? <Shimmer /> : (
+    <div className="body my-[20px]">
+      <div className="filter flex">
+        <div className="m-[15px] p-[15px]">
+          <input type="text" value={searchText} onChange={(e)=>{
+            setSearchText(e.target.value)
+          }}/>
 
-  return(
-    <div className="body">
-      <div>
-        <div className="m-[15px] p-[15px]">Search</div>
+          <button onClick={()=>{
+            // filter the restuarants cards and update the UI
+            // console.log('do filtering on:', searchText)
+            const filteredData = listOfRestaurants.filter((currentRes)=>{
+              return currentRes.info.name.toLowerCase().includes(searchText.toLowerCase())
+            })
+
+            setFilteredRestaurant(filteredData)
+
+          }}>Search</button> 
+          </div>
         <button className="btn bg-blue-500 text-white px-4 py-2 rounded" onClick={()=>{
           let filterRes = listOfRestaurants.filter((res)=>{
             return res.info.avgRating > 4.5;
           });
 
-          setListOfRestaurants(filterRes)
+          setFilteredRestaurant(filterRes)
           
         }}>Top Rated Restaurants</button>
 
       </div>
       <div className="flex flex-wrap m-[20px]">
-        { listOfRestaurants.map((currentRestaurant) => <RestaurantCard key={currentRestaurant.info.id} resObj = {currentRestaurant}/>)}
+        { filteredRestaurant.map((currentRestaurant) => <RestaurantCard key={currentRestaurant.info.id} resObj = {currentRestaurant}/>)}
       </div>
     </div>
   )
