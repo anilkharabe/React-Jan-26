0a49521fb80254c7fcc2f5f59fc1d1599efbec05
Aniruddh <anilkharabe@gmail.com>
2026-04-07 09:03:44 +0530

    different types of useEffect, routing, routing configuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,61 @@
-import { useEffect, useState } from 'react';
-import {LOGO_URL} from '../utils/constant';
+import { useEffect, useState } from "react";
+import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 
-const Header = ()=>{
-  const [btnName, setBtnName] = useState('Flip me');
-  
-    return(
-      
+const Header = () => {
+  const [btnName, setBtnName] = useState("Flip me");
+  const [loginBtn, setLoginBtn] = useState("Login");
+
+
+  // if no dependency array => useEffect is called on every render
+  useEffect(()=>{
+    console.log('useEffect called with no dependency');
+  })
+
+  // if dependecy array is empty => useEffect is called on initial render(Just once)
+  useEffect(()=>{
+    console.log('useEffect called with empty dependecy array');
+  }, [])
+
+
+    // if dependecy array is [btnName] => useEffect is called on everytime btnName is changing
+  useEffect(()=>{
+    console.log('useEffect called with  dependecy array');
+  }, [btnName])
+
+  return (
     <div className="flex justify-between border-solid items-center">
-      <div className='logo-container'>
-          <img className="w-[170px] ml-3" src={LOGO_URL}></img>
+      <div className="logo-container">
+        <img className="w-[170px] ml-3" src={LOGO_URL}></img>
       </div>
       <div className="nav-items">
-        <ul className='flex '>
-          <li className='list-none p-[15px] m-[15px]'>
-              Home
-          </li>
-          <li className='list-none p-[15px] m-[15px]'>
-              About
-          </li>
-          <li className='list-none p-[15px] m-[15px]'>
-              Contact Us
-          </li>
-          <li className='list-none p-[15px] m-[15px]'>
-              Cart
-          </li>
-          <button onClick={()=>{
-            btnName === 'Flip me' ?  setBtnName('Head') :  setBtnName('Flip me');
-          }}>{btnName}</button>
+        <ul className="flex ">
+          <li className="list-none p-[15px] m-[15px]"><Link to="/">Home</Link> </li>
+          <li className="list-none p-[15px] m-[15px]"><Link to="/about">About</Link><></></li>
+          <li className="list-none p-[15px] m-[15px]"><Link to="/contact">Contact Us</Link> </li>
+          <li className="list-none p-[15px] m-[15px]">Cart</li>
+          <button
+            onClick={() => {
+              btnName === "Flip me"
+                ? setBtnName("Head")
+                : setBtnName("Flip me");
+            }}
+          >
+            {btnName}
+          </button>
+           <button
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,16 @@
+const RestaurantMenu = ()=>{
+    return (
+        <div>
+            <h1>Name of Restaurant</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Burger</li>
+                <li>Vadapav</li>
+                <li>Pizza</li>
+            </ul>
+        </div>
+    )
+}
+
+export default RestaurantMenu;
