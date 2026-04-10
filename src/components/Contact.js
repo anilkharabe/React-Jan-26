39a392664a377ce2aba5af9c8a854177de353546
Aniruddh <anilkharabe@gmail.com>
2026-04-10 08:54:57 +0530

    life cycle of react class based component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,30 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const Contact = ()=>{
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+
     return (
-        <div>
-            <h1>Contact</h1>
-            <h2>This is Contact page</h2>
-            <User name={"Aniruddha - Functional component"} city={"Pune"}/>
-            <UserClass name={"Aniruddha - Class component"} city={"Hyderabad"}/>
-        </div>
-    )
+      <div>
+        <h1>Contact</h1>
+        <h2>This is Contact page</h2>
+        {/* <User name={"Aniruddha - Functional component"} city={"Pune"}/> */}
+        <UserClass name={"First"} city={"Hyderabad"} />
+        {/* <UserClass name={"Second"} city={"Pune"} /> */}
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,18 +1,44 @@
+import axios from "axios";
 import { Component } from "react";
 class UserClass extends Component {
   constructor(props) {
     super(props);
 
-    // reserved object
+    // reserved object - big object
     this.state = {
       count: 0,
-      count2: 1
+      count2: 1,
+      name:'',
+      city:''
     };
+    console.log("child Constructor");
+  }
+
+  async componentDidMount(){
+    console.log('Child Component Did Mount')
+    const data = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+    
+    //changing the state
+    this.setState({
+        name: data.data.name,
+        city: data.data.address.city
+    })
+  }
+
+  // homework: what is use of componentDidUpdate()  ??
+  componentDidUpdate(){
+    console.log('child component Did update');
+  }
+
+  componentWillUnmount(){
+    console.log('child component will unmount');
   }
 
   render() {
-    const { name, city } = this.props;
-    const { count, count2 } = this.state;
+    
+    console.log("Child Render");
+    // const { name, city } = this.props;
+    const { count, count2, name, city } = this.state;
 
     return (
       <div className="border-solid">
